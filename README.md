aab8b549b0ecc95a1f9d1279c8ef37421186c276
Mark Lopez <mark_lopez@icloud.com>
2021-06-05 23:59:47 -0500

    final pushes of what I completed

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # weather-app
+
+<p>This application allows you to search a city to view the weather forecast.</p>
+
+<img src= "screenshot.png">
+
+Application URL:
+https://markl0218.github.io/weather-app/
+
+github repo:
+https://github.com/markl0218/weather-app
--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-var APIKey = "821cccc3bd68e40b96f7c4d8b9907cd8"
+// created variables to use with api to display info
+var searchButton = document.querySelector('searchButton')
+var searchBar = document.querySelector('searchBar')
+var cityName = document.querySelector('cityName')
+var temp = document.querySelector('temp')
+var details = document.querySelector('details')
+
+searchButton.addEventListener('click',function(){
+fetch('api.openweathermap.org/data/2.5/weather?q='+searchBar.value+'&appid=821cccc3bd68e40b96f7c4d8b9907cd8')
+.then(response => response.json())
+.then(data => console.log(data))
+})
